fe94fff66e57c6e18c4a5e9914b4e31e2c127812
WindowsPhoneDeveloper <seo.polhovskiy@gmail.com>
2014-08-01 16:55:53 +0300

    added category controller

--- src/main/java/com/polkhovskyi/appstore/controllers/CategoryController.java
+++ src/main/java/com/polkhovskyi/appstore/controllers/CategoryController.java
@@ -1,11 +1,34 @@
 package com.polkhovskyi.appstore.controllers;
 
+import com.polkhovskyi.appstore.dto.Category;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by apolkhovskiy on 01.08.2014.
  */
 @Controller
 public class CategoryController {
+    @RequestMapping("/category")
+    public String category(Model model) {
+        List<Category> myList = new ArrayList<Category>();
+        Category temp = new Category();
+        temp.title = "Category 1";
+        myList.add(temp);
 
+        temp = new Category();
+        temp.title = "Category 2";
+        myList.add(temp);
+
+        temp = new Category();
+
+        myList.add(temp);
+        model.addAttribute("name", myList);
+        return "category";
+    }
 }
--- src/main/java/com/polkhovskyi/appstore/dto/Category.java
+++ src/main/java/com/polkhovskyi/appstore/dto/Category.java
@@ -4,5 +4,15 @@ package com.polkhovskyi.appstore.dto;
  * Created by apolkhovskiy on 01.08.2014.
  */
 public class Category {
+    public String title;
 
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
 }
